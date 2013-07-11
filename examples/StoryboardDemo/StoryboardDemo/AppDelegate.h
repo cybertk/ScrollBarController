5f0108aa3d3d6365a5fb6c339ac493fef626ad3d
Quanlong He <quanlongx.he@intel.com>
2013-07-12 02:58:16 +0800

    Add StoryboardDemo example.

---
+++ examples/StoryboardDemo/StoryboardDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StoryboardDemo
+//
+//  Created by Quanlong He on 7/12/13.
+//  Copyright (c) 2013 Quanlong He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ examples/StoryboardDemo/StoryboardDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  StoryboardDemo
+//
+//  Created by Quanlong He on 7/12/13.
+//  Copyright (c) 2013 Quanlong He. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
